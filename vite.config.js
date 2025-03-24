c609c3e9e0842e97967cf8df3b5226973936ec21
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-24 12:38:29 +0100

    setup project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), autoprefixer()],
   resolve: {
     alias: {
       '@': '/src'
