554e47b3c3c70df09a3f209c44d82b684e643434
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-24 23:11:02 +0100

    define routes for login and register pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/auth/login',
+      name: 'auth-login',
+      component: LoginView,
+    },
+    {
+      path: '/auth/register',
+      name: 'auth-register',
+      component: RegisterView,
     }
   ],
 })
