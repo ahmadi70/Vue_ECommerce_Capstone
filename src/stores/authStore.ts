085cdb7abb71a26ab41db60f6cf2a5f4bc8551f4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-25 00:52:28 +0100

    update registerUser to use types

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,21 +1,22 @@
 import { defineStore } from "pinia"
 import axios from "@/plugins/axios"
+import type { User, APIResponse } from "@/types"
 
 export const useAuthStore = defineStore('AuthStore', {
   state: () => ({}),
 
   actions: {
     async registerUser(form: Record<string, string>) {
-      return new Promise<string>(async (resolve, reject) => {
+      return new Promise<User>(async (resolve, reject) => {
         try {
-          const { data } = await axios.post('/users/register', {
+          const { data } = await axios.post<APIResponse<{ user: User }>>('/users/register', {
             ...form
           })
-          console.log('Register ', data)
+          console.log('Register ', data.data)
 
-          resolve('')
+          resolve(data.data.user)
         } catch (error) {
-          reject()
+          reject(error)
         }
       })
     }
