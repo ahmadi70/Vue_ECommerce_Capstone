3bf4a2debcf1d8b6c82226d6d4be2a99e55ef913
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-24 14:40:45 +0100

    install ui components using shadcn-vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <div>Home page</div>
+  <div>
+    <h1>Home page</h1>
+    <Button variant="destructive">Click me</Button>
+  </div>
 </template>
