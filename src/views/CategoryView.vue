f6e0b084271b1e39b967234c4ab6fbb5dd5df7f1
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-25 16:34:58 +0100

    create category component

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import {
+  Card,
+  CardContent,
+  CardFooter
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useCategoryStore } from '@/stores/categoryStore'
+
+type PAYLOAD = {
+  name: string
+}
+
+const form = ref<PAYLOAD>({
+  name: 'category 1'
+})
+
+const router = useRouter()
+const categoryStore = useCategoryStore()
+
+const onSubmit = async () => {
+  try {
+    await categoryStore.createCategory(form.value)
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  } finally {
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center min-h-screen">
+    <div class="mx-auto w-full max-w-md">
+      <form @submit.prevent="onSubmit">
+        <Card class="overflow-y-auto pt-4">
+          <CardContent class="grid gap-4">
+            <div class="grid gap-2">
+              <Label for="name">Category Name</Label>
+              <Input 
+                id="name"
+                type="text"
+                placeholder="name"
+                v-model="form.name"
+              />
+            </div>
+          </CardContent>
+          <CardFooter class="flex-col space-y-2">
+            <Button class="w-full" type="submit"> Create </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </div>
+  </div>
+</template>
