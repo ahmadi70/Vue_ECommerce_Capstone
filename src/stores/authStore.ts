73053f6959331b2c8863664696515df442b629c4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-25 11:49:54 +0100

    define login action

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia"
 import axios from "@/plugins/axios"
 import type { User, APIResponse } from "@/types"
 
-export const useAuthStore = defineStore('AuthStore', {
+export const useAuthStore = defineStore('authStore', {
   state: () => ({}),
 
   actions: {
@@ -13,6 +13,20 @@ export const useAuthStore = defineStore('AuthStore', {
             ...form
           })
           console.log('Register ', data.data)
+
+          resolve(data.data.user)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    async loginUser(form: Record<string, string>) {
+      return new Promise<User>(async (resolve, reject) => {
+        try {
+          const { data } = await axios.post<APIResponse<{ user: User, accessToken:string, refreshToken:string }>>('/users/login', {
+            ...form
+          })
+          console.log('Login ', data.data)
 
           resolve(data.data.user)
         } catch (error) {
