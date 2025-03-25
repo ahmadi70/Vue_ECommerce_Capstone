6cd8b476a6ce5701fa9e69f07fde9553dda806e8
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-25 16:32:12 +0100

    update axios to get token

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -4,4 +4,19 @@ const axiosInstance = axios.create({
   baseURL: 'https://api.freeapi.app/api/v1'
 })
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = JSON.parse(localStorage.getItem('currentAuthTokens') || '{}')
+
+  if(token) {
+    config.headers = config.headers || {}
+    const unprotectedURLs = ['/users/login/', '/users/register/']
+
+    if(config.url && !unprotectedURLs.includes(config.url)) {
+      config.headers.Authorization = token ? `Bearer ${token.accessToken}` : ''
+    }
+  }
+
+  return config
+})
+
 export default axiosInstance
