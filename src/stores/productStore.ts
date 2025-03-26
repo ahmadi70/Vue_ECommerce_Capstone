9d025a873f3f0a48083a25d8a9898a9c7e0e39c8
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-26 19:32:09 +0100

    create productStore

---
+++ src/stores/productStore.ts
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia"
+import axios from "@/plugins/axios"
+import type { APIResponse, Product, Products } from "@/types"
+
+export const useProductStore = defineStore('productStore', {
+  state: () => ({
+    productsData: {} as Products
+  }),
+
+  actions: {
+    async createProduct(form: Record<string, any>) {
+      return new Promise<Product>(async (resolve, reject) => {
+        try {
+          const formData = new FormData()
+          formData.append('name', form.name)
+          formData.append('price', form.price)
+          formData.append('stock', form.stock)
+          formData.append('mainImage', form.mainImage)
+          formData.append('description', form.description)
+          formData.append('category', form.category)
+
+          for (const image of form.subImages) {
+            formData.append('subImages', image)
+          }
+
+          const { data } = await axios.post<APIResponse<Product>>('/ecommerce/products', formData)
+
+          console.log('Products ', data.data)
+          resolve(data.data)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+  }
+})
