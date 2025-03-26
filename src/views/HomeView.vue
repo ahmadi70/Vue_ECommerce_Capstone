eae115bb85006aad569ebe952e90e866dc3725df
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-26 15:18:00 +0100

    update HomeView to use product modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { Button } from '@/components/ui/button'
+import ProductModal from '@/components/ProductModal.vue'
+import useProductModal from '@/composables/useProductModal'
+import { useCategoryStore } from '@/stores/categoryStore'
+import { useGlobalLoader } from 'vue-global-loader'
+
+const { isOpen, onOpen } = useProductModal()
+
+const { displayLoader, destroyLoader } = useGlobalLoader()
+const categoryStore = useCategoryStore()
+const fetchCategories = async () => {
+  try {
+    displayLoader()
+    await categoryStore.getCategories(1, 5)
+  } catch (error) {
+    console.log(error)
+  } finally {
+    destroyLoader()
+  }
+}
+
+onMounted(async () => {
+  await fetchCategories()
+})
 </script>
 
 <template>
-  <div>
-    <h1>Home page</h1>
-    <Button variant="destructive">Click me</Button>
+  <ProductModal v-if="isOpen" />
+  <div class="mx-auto w-full max-w-4xl my-10">
+    <div class="flex justify-between items-center">
+      <h1 class="text-2xl font-bold">All Products</h1>
+      <Button @click="onOpen">Add Product</Button>
+    </div>
   </div>
 </template>
