2fa23eb592eea1b5bf1a1ada4a5ae28d111f9837
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-27 02:11:32 +0100

    define route for single product

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import CategoryView from '@/views/CategoryView.vue'
+import SingleProduct from '@/views/products/[id].vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/products/:id',
+      name: 'single-product',
+      component: SingleProduct,
     },
     {
       path: '/category',
