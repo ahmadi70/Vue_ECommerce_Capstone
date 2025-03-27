7d6db5198295fdb5b6d2ad33c3c67e70be83ea5f
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-27 02:08:52 +0100

    define getProduct, editProduct and deleteProduct

--- src/stores/productStore.ts
+++ src/stores/productStore.ts
@@ -44,6 +44,59 @@ export const useProductStore = defineStore('productStore', {
           reject(error)
         }
       })
+    },
+    async getProduct(productId: string) {
+      return new Promise<Product>(async (resolve, reject) => {
+        try {
+          const { data } = await axios.get<APIResponse<Product>>(`/ecommerce/products/${productId}`)
+
+          console.log('Product', data.data)
+          resolve(data.data)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    async editProduct(productId: string, form: Record<string, any>) {
+      return new Promise<Product>(async (resolve, reject) => {
+        try {
+          const formData = new FormData()
+          formData.append('name', form.name)
+          formData.append('price', form.price)
+          formData.append('stock', form.stock)
+          formData.append('description', form.description)
+          formData.append('category', form.category)
+          if(form.mainImage) {
+            formData.append('mainImage', form.mainImage)
+          }
+          if(form.subImages) {
+            for (const image of form.subImages) {
+              formData.append('subImages', image)
+            }
+          }
+
+          const { data } = await axios.patch<APIResponse<Product>>(`/ecommerce/products/${productId}`, formData)
+
+          await this.getProducts(1, 2)
+          console.log('Product', data.data)
+          resolve(data.data)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    async deleteProduct(productId: string) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const { data } = await axios.delete(`/ecommerce/products/${productId}`)
+
+          await this.getProducts(1, 2) //Improve - need to change for production
+          console.log('Products', data.data)
+          resolve(data.data)
+        } catch (error) {
+          reject(error)
+        }
+      })
     }
   }
 })
